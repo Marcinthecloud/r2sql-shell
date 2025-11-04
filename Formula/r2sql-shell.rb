e5928d177893ed1a93bd730887d29d53a7f01f37
Marc Selwan <marc@marcinthecloud.com>
2025-11-04 10:35:46 -0800

    update url

--- Formula/r2sql-shell.rb
+++ Formula/r2sql-shell.rb
@@ -1,7 +1,7 @@
 class R2sqlShell < Formula
   desc "Interactive shell for querying R2 Data Catalog with R2 SQL"
   homepage "https://github.com/marcinthecloud/r2sql-shell"
-  url "https://github.com/marcinthecloud/r2sql-shell/releases/download/v1.0.0/r2sql-shell-macos"
+  url "https://github.com/marcinthecloud/r2sql-shell/releases/download/1.0.0/r2sql-shell-macos"
   sha256 "326c722240c05460f0651e1e2746ac62bf8d1dbdd726eb790c10ee3b47884364"  # Run: shasum -a 256 bin/r2sql-shell-macos
   version "1.0.0"
   license "MIT"
